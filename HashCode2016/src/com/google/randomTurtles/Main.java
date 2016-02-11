7664bfee72a035f8208ca420543f79c85bc2cb09
Simon Narduzzi <simon.narduzzi@epfl.ch>
2016-02-11 18:32:17 +0100

    Created main

---
+++ HashCode2016/src/com/google/randomTurtles/Main.java
@@ -0,0 +1,10 @@
+package com.google.randomTurtles;
+
+/**
+ * Created by Simon on 11.02.2016.
+ */
+public class Main {
+    public static void main(String []args){
+        System.out.print("Hello RandomTurtles");
+    }
+}
