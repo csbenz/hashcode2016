68e1327ce44bbbc8c2700dc17273fa2c4e8a34ba
csbenz <christopher.benz@epfl.ch>
2016-02-11 20:12:56 +0100

    Main

--- HashCode2016/src/com/google/randomTurtles/Main.java
+++ HashCode2016/src/com/google/randomTurtles/Main.java
@@ -1,10 +1,15 @@
 package com.google.randomTurtles;
 
+
 /**
  * Created by Simon on 11.02.2016.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello RandomTurtles");
+
+        Parser.parse("mother_of_all_warehouses.in");
+
+
     }
 }
