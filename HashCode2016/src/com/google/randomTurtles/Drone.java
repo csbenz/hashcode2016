f3cbe7dc6880d2e4455986447522d9b5cffc4614
Simon Narduzzi <simon.narduzzi@epfl.ch>
2016-02-11 22:13:12 +0100

    Modified drone

---
+++ HashCode2016/src/com/google/randomTurtles/Drone.java
@@ -0,0 +1,13 @@
+package com.google.randomTurtles;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+/**
+ * Created by Simon on 11.02.2016.
+ */
+public class Drone {
+    ArrayList<Item> itemList;
+
+}
