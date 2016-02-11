f92a95fa5645c4ff2557752b60e85b31ba2216e3
Alexandre Connat <alexandre.connat@epfl.ch>
2016-02-11 19:30:47 +0100

    Create World Class

---
+++ HashCode2016/src/com/google/randomTurtles/World.java
@@ -0,0 +1,23 @@
+package com.google.randomTurtles;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by Alexandre on 11.02.16.
+ */
+public class World {
+
+    private final int rows;
+    private final int columns;
+    private final int D; // Drone Number
+    private final long deadline;
+    private final int droneMaxLoad;
+
+    private final ArrayList<Integer> products;
+
+
+    public World() {
+
+    }
+}
