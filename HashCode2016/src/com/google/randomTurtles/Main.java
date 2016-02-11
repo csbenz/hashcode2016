b71f87fc2a640cf02a57f3489a7cf83532638c18
csbenz <christopher.benz@epfl.ch>
2016-02-11 19:26:53 +0100

    added files lol

--- HashCode2016/src/com/google/randomTurtles/Main.java
+++ HashCode2016/src/com/google/randomTurtles/Main.java
@@ -4,7 +4,7 @@ package com.google.randomTurtles;
  * Created by Simon on 11.02.2016.
  */
 public class Main {
-    public static void main(String []args){
+    public static void main(String[] args) {
         System.out.print("Hello RandomTurtles");
     }
 }
